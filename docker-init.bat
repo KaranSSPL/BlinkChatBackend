d4ab7f4d9b1ef506d068d29dc8d5db3a80c804dd
Karan <96464179+KaranSSPL@users.noreply.github.com>
2025-05-05 19:34:25 +0530

    Update docker compose.

--- docker-init.bat
+++ docker-init.bat
@@ -1,7 +1,7 @@
 docker stop karansspl2021/blinkchatbackend
 docker rm karansspl2021/blinkchatbackend
 docker rmi karansspl2021/blinkchatbackend
-docker build -t karansspl2021/blinkchatbackend .
-docker push karansspl2021/blinkchatbackend
-docker pull karansspl2021/blinkchatbackend
+docker build -t karansspl2021/blinkchatbackend:v1 .
+docker push karansspl2021/blinkchatbackend:v1
+docker pull karansspl2021/blinkchatbackend:v1
 ::docker run -d -p 5273:80 --network redis-network -v D:\docker-volume\blinkchat\backend:/app/wwwroot/models -e ASPNETCORE_ENVIRONMENT=development -e LM__licensekey=019B0C0-100235-C295A6-032129-B2D000-000525-049078-406173-9113D7-A2 -e ConnectionStrings__Redis=host.docker.internal:6379 -e ASPNETCORE_URLS=http://+:80 -e ASPNETCORE_HTTPS_PORT=5273 --name blinkchatbackend karanjangid/blinkchatbackend:latest
